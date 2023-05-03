4dee775a35802e2f4ac3acf037fcc5b06d2feb9d
Eyimofe Omotayo <eyimofeomotayo@gmail.com>
2023-05-03 23:56:17 +0100

    Chat header ui and online status

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,41 +1,12 @@
 import * as React from 'react';
 
 import { StyleSheet, Text, View } from 'react-native';
-import { Bubble } from 'react-native-chat';
+import { Bubble, Chat } from 'react-native-chat';
 
 export default function App() {
-  const date = new Date();
-
   return (
     <View style={styles.container}>
-      <Bubble
-        username="John"
-        backgroundColor="#00008B"
-        color="#fff"
-        time={date.getTime()}
-      >
-        <Text style={{ color: '#fff', fontWeight: '500' }}>
-          Mofe is so cool
-        </Text>
-      </Bubble>
-      <Bubble
-        imageUrl="https://knittingisawesome.com/wp-content/uploads/2012/12/cat-wearing-a-reindeer-hat1.jpg"
-        backgroundColor="#00008B"
-      >
-        <Text>😃</Text>
-      </Bubble>
-      <Bubble
-        time={date.getTime()}
-        text={` Hello this is an example of the ParsedText, links like http://www.google.com or http://www.facebook.com are clickable and phone number 444-555-6666 can call too.
-        But you can also do more with this package, for example Bob will change style and David too. foo@gmail.com
-        And the magic number is 42!
-        #react #react-native)`}
-        username="Mofe"
-        color="#fff"
-        backgroundColor="#AA4A44"
-        position="right"
-        wrapperStyle={{ right: { backgroundColor: '#ccc' } }}
-      />
+      <Chat username="Eyimofe Omotayo" isOnline={false} />
     </View>
   );
 }
